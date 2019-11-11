8186e684c0b7f52ee404d3e57d95d3543bd37a12
tony <aamoesi@gmail.com>
2019-11-10 20:06:21 -0500

    logo

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
-import React from 'react'
-import {render} from 'react-testing-library'
-import App from './App'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
 it('renders without crashing', () => {
-  const {getByText} = render(<App />)
-  expect(getByText('Learn React')).toBeInTheDOM()
-})
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
