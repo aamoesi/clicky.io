9db2e2ca36b852c01919ab7ff292a43ff86d8752
tony <aamoesi@gmail.com>
2019-11-12 20:41:26 -0500

    deploy

---
+++ build/precache-manifest.597050fdd6b5650f24bc85460c9937b0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c63a26115c09f9ec32436ca804a3163",
+    "url": "/clicky.io/./index.html"
+  },
+  {
+    "revision": "0477764a23d8c5e7f45c",
+    "url": "/clicky.io/./static/css/main.7e0ad50f.chunk.css"
+  },
+  {
+    "revision": "62a42db3f9914a028161",
+    "url": "/clicky.io/./static/js/2.2570cf70.chunk.js"
+  },
+  {
+    "revision": "0477764a23d8c5e7f45c",
+    "url": "/clicky.io/./static/js/main.619e397c.chunk.js"
+  },
+  {
+    "revision": "ec08912bb03a94aba3db",
+    "url": "/clicky.io/./static/js/runtime-main.dcfc4df4.js"
+  }
+]);
